283f0207a9a546c6bab672481df0e5a4d148146b
Abhay Mahendera <81372552+AbhayMahendera@users.noreply.github.com>
2023-11-12 13:13:40 -0500

    done

--- assgn3/app.js
+++ assgn3/app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const fs = require("fs");
 const cookieParser = require('cookie-parser');
 const ejs = require('ejs');
-
+const apiRouter = require('./routes/apiRouter');
 // Create an Express application
 const app = express();
 const port = 8000;
