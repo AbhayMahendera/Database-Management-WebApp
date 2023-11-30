9712b8e2d57e7c9a9cc959c9d9b056ebbd1bd534
Abhay Mahendera <81372552+AbhayMahendera@users.noreply.github.com>
2023-11-30 14:55:06 -0500

    shifted every login related things to database

--- assgn4/sequelize.js
+++ assgn4/sequelize.js
@@ -21,7 +21,34 @@ sequelize
     console.log('Unable to connect to the database:', err);
   });
 
+
+
 //    ----------------------------- ATTRIBUTES FOR USERS TABLE -----------------------------    //
+
+
+
+const admins = sequelize.define('admins', {
+  firstname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true,
+  },
+  lastname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  timestamps: false,
+});
+
 
 const users = sequelize.define('users', {
   id: {
@@ -96,34 +123,69 @@ sequelize.sync()
 
 //    ----------------------------- READ -----------------------------    //
 
+
+
+
 function readId(id) {
   users.findByPk(id)
-      .then((user) => {
+      .then((user) => {         // i did logged them to console to check for errors
           if (user) {
-              console.log('User details for ID', id);
-              console.log('First Name:', user.firstname);
-              console.log('Last Name:', user.lastname);
-              console.log('Email:', user.email);
-              console.log('Password:', user.password);
-              console.log('Date of Birth:', user.dob);
-              console.log('Phone:', user.phone);
+              //console.log('User details for ID', id);
+              //console.log('First Name:', user.firstname);
+              //console.log('Last Name:', user.lastname);
+              //console.log('email:', user.email);
+              //console.log('Password:', user.password);
+              //console.log('Date of Birth:', user.dob);
+              //console.log('Phone:', user.phone);
+              console.log (' -------------> READ DATA - SUCCESS !')
           } else {
-              console.log('User not found with ID', id);
+              console.log(' -------------> NO USER FOUND WITH ID ', id);
           }
       })
       .catch((error) => {
-          console.error('Error fetching user details:', error);
+          console.error(' -------------> READ DATA - FAILURE ! : ', error);
       });
 
 } 
-  // Call the readId function
-  readId();
 
+// --------------> LOGIN
+async function loginVerify(email, password, userType) {
+  try {
+    console.log('----------------------------');
+    console.log('login request for : ');
+    console.log('email:', email);
+    console.log('Password:', password);
 
-  // ----> ADMIN | SEARCH
+    let user;
+    if (userType === 'admins') {
+      user = await admins.findOne({
+        where: {
+          email: email,
+          password: password,
+        },
+      });
+    } else if (userType === 'user') {
+      user = await users.findOne({
+        where: {
+          email: email,
+          password: password,
+        },
+      });
+    }
 
-  
-
+    if (user) {
+      // If user is found, return true to indicate successful login
+      return true;
+    } else {
+      // If user is not found, return false to indicate unsuccessful login
+      return false;
+    }
+  } catch (error) {
+    // Handle any errors that occur during the verification process
+    console.error('Error during login verification:', error);
+    return false;
+  }
+}
 
 //    ----------------------------- UPDATE -----------------------------    //
 
@@ -151,4 +213,6 @@ module.exports = {
   sequelize,
   users,
   readId,
+  loginVerify,
+  admins,
 };
